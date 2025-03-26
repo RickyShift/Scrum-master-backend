2b02fbd1f6116d9a293957e875c2c1b52a7a07f7
RickyShift <rcdra2000@gmail.com>
2025-03-26 15:14:45 +0000

    new build

--- src/main/java/gpi/scrum/controller/UserStoryController.java
+++ src/main/java/gpi/scrum/controller/UserStoryController.java
@@ -15,7 +15,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/userStory")
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "http://localhost:8080")
 public class UserStoryController {
 
     private final UserStoryService userStoryService;
